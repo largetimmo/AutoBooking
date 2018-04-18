efe14687cced9ef72ea4563fd7f79eb24d935ff6
largetimmo <kylechen49@gmail.com>
2018-04-18 17:14:36 -0400

    Add tostring

--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -35,4 +35,13 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
