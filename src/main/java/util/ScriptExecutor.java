8fa32b4f8f5a8a220ec7848ba4a4640e9cfa0f3a
largetimmo <kylechen49@gmail.com>
2018-04-20 15:09:58 -0400

    Able load lib

--- src/main/java/util/ScriptExecutor.java
+++ src/main/java/util/ScriptExecutor.java
@@ -1,16 +1,32 @@
 package util;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ScriptExecutor {
 
-    public ScriptExecutor(){
+    public ScriptExecutor() throws IOException {
         /**
          * 1.load compiled lib
          * 2.create file in temp dir
          * 3.copy lib to temp dir
          * 4.load the lib in temp file
          */
-
+        InputStream inputStream = getClass().getResourceAsStream("/util/libmiddleware.so");//load lib as text
+        File tempf = File.createTempFile("libmiddleware", "so");
+        FileOutputStream fos = new FileOutputStream(tempf);//establish pipe to tempf
+        int length = 0;
+        byte[] buffer = new byte[1024];
+        while ((length = inputStream.read(buffer)) != -1) {
+            fos.write(buffer, 0, length);
+        }
+        inputStream.close();
+        fos.close();
+        System.load(tempf.getAbsolutePath());
     }
+
     public native String execute(String filename, String[] arg);
 
 }
